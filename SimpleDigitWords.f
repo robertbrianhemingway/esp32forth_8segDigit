c069192f013d0ce9f804b18d024207e2884ede4f
robertbrianhemingway <23691761+robertbrianhemingway@users.noreply.github.com>
2022-12-18 08:50:11 +1000

    Update SimpleDigitWords.f

--- SimpleDigitWords.f
+++ SimpleDigitWords.f
@@ -17,6 +17,16 @@
 : eight  ( -- ) allSegmentsOn ;
 : nine  ( -- ) allSegmentsOff .a .f .g .b .c ; 
 
+: .1 ( -- ) one ;
+: .2 two ;
+: .3 three ;
+: .4 four ;
+: .5 five ;
+: .6 six ;
+: .7 seven ;
+: .8 eight ;
+: .9 nine ;
+
 : setPinHigh  ( n -- ) high digitalWrite ;
 : setPinLow   ( n -- ) low  digitalWrite ;  
 : allSegmentsOff  ( -- )  \ set all except 3,8 to HIGH
